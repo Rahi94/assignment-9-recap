1c76d74dfc57bdc9ae1f797fe8badcf830da5dea
tasib94 <rahi84974@gmail.com>
2024-08-08 01:22:23 +0600

    navbar created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../../components/Navbar';
 
 const Home = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <p className="text-3xl">This is home page!</p>
         </div>
     );
