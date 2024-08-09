d403a3e6fa3fc7771ebb8c08ae39d399b7c80eb6
tasib94 <rahi84974@gmail.com>
2024-08-10 02:04:42 +0600

    form validation completed

--- src/components/firebaseProvider/FirebaseProvider.jsx
+++ src/components/firebaseProvider/FirebaseProvider.jsx
@@ -1,10 +1,19 @@
 import React, { createContext } from 'react';
-
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import auth from '../../firebase/firebase.config';
 export const AuthContext = createContext(null);
+
+// create user
+const createUser = (email, password)=>{
+    return createUserWithEmailAndPassword(auth, email, password)
+
+}
 
 const FirebaseProvider = ({children}) => {
 
-    const allValue = {name: 'test'};
+    const allValue = {
+        createUser
+    };
     return (
         <AuthContext.Provider value={allValue}>
             {children}
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Navbar from '../../components/Navbar';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../components/firebaseProvider/FirebaseProvider';
+import { useForm } from 'react-hook-form';
 
 const Register = () => {
+
+        const {createUser} = useContext(AuthContext)
+        console.log(createUser)
+
+        const {
+            register,
+            handleSubmit,
+            watch,
+            formState: { errors },
+          } = useForm()
+          const onSubmit = (data) => console.log(data)
+
     return (
         <div>
             <Navbar></Navbar>
@@ -14,18 +28,32 @@ const Register = () => {
 
                     </div>
                     <div className="card mt-6 w-1/3 bg-base-100  shrink-0 shadow-2xl">
-                        <form className="card-body">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" placeholder="Full name" className="input input-bordered" required 
+                                 {...register("FullName", { required: true })}
+                                />
+                                {errors.FullName && <span className='text-red-500'>This field is required</span>}
+                                
+                            </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
+                                <input type="email" placeholder="Email" className="input input-bordered" required 
+                                {...register("Email", { required: true })}
+                                />
+                                {errors.Email && <span className='text-red-500'>This field is required</span>}
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input type="password" placeholder="password" className="input input-bordered" required 
+                                {...register("Password", { required: true })}
+                                />
+                                {errors.Password && <span className='text-red-500'>This field is required</span>}
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Already have an account? please <Link className='text-blue-600 font-bold' to='/login'>Login</Link></a>
                                 </label>
